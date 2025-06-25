f9bd3042762438ce7ccf807db0f1770a441f5671
Miguel Angel Fernandez <miguelangelfb19@utp.edu.co>
2025-06-25 09:40:20 -0500

    customer testimonies finish

---
+++ src/constants/costumers.ts
@@ -0,0 +1,17 @@
+export const costumersComments: { name: string; comment: string }[] = [
+  {
+    name: "Harry Hurtado",
+    comment:
+      "Excelente servicio. Confianza, amabilidad, respeto, agenda con tiempo, y lo atienden a la hora progamada.",
+  },
+  {
+    name: "Mauricio Orozco",
+    comment:
+      "Necesitaba corregir la inclinación de la rueda trasera izquierda y quedó perfecta, inclinación de cero grados después de salir de allí. Muy recomendado este banco de prueba.",
+  },
+  {
+    name: "Antonio José García Ramirez",
+    comment:
+      "Excelente servicio puntual ágil y eficiente ,muy honrado, excelente trabajo lo recomiendo totalmente",
+  },
+];
