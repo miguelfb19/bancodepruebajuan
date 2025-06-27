382ac4aa1acb3eaf669266b88a5fc45a31ba1394
Miguel Angel Fernandez <miguelangelfb19@utp.edu.co>
2025-06-27 11:28:58 -0500

    fix SEO and adding breadcrubs and FAQ section

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,21 @@ export default defineConfig({
       plugins: [tailwindcss()],
   },
 
-  integrations: [react()]
+  integrations: [
+    react(), 
+    sitemap({
+      customPages: [
+        'https://bancodepruebajuan.com/',
+        'https://bancodepruebajuan.com/about',
+        'https://bancodepruebajuan.com/contact'
+      ],
+      i18n: {
+        defaultLocale: 'es',
+        locales: {
+          es: 'es-CO'
+        }
+      }
+    })
+  ],
+  site: 'https://bancodepruebajuan.com',
 });
